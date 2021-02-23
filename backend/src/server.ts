7bb4cd629de3fd880056a1dca1c23f15d313a80b
Ronie Peterson <roniepetersondf@gmail.com>
2021-02-23 19:40:59 -0300

    dia 2 nlw

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
-import express from "express"
+import 'reflect-metadata'
+import express, { response } from "express"
+import "./database"
+import { router } from './routes'
+
 const app = express()
+app.use(express.json())
+app.use(router)
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World - NLW04"})
-})
-
-app.post("/", (request, response) => {
-    return response.json({ message: "Os dados foram salvos com sucesso!"})
+app.get('/', (request, response) => {
+    return response.send('oi')
 })
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
