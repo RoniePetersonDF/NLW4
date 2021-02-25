27272e8ce183ae1526717c470ffa87daafc3ce7a
roniepetersondf <roniepetersondf@gmail.com>
2021-02-24 21:08:23 -0300

    nlw4 - dia 2 54 min

---
+++ backend/src/__tests__/First.test.ts
@@ -0,0 +1,14 @@
+describe("First", () => {
+    
+    it("deve ser possivel somar dois numeros", () => {
+
+        expect(2+2).toBe(4)
+
+    })
+
+    it("deve ser possivel somar dois numeros", () => {
+
+        expect(2+2).not.toBe(5)
+
+    })
+})
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,3 @@
-import 'reflect-metadata'
-import express, { response } from "express"
-import "./database"
-import { router } from './routes'
-
-const app = express()
-app.use(express.json())
-app.use(router)
-
-app.get('/', (request, response) => {
-    return response.send('oi')
-})
+import { app } from "./app";
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
