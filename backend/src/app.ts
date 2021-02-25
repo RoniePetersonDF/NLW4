6e580583170dc8ea70137ad6f7a21fdc7533b6c1
Ronie Peterson <roniepetersondf@gmail.com>
2021-02-25 15:49:25 -0300

    NLW4 - Terceiro dia  testes falhando no windows

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,9 @@ import 'reflect-metadata'
 import express, { response } from "express"
 import "./database"
 import { router } from './routes'
+import { createConnection } from 'typeorm'
 
+createConnection()
 const app = express()
 app.use(express.json())
 app.use(router)
